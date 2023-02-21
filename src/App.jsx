5a2378da30734e12978aa8a335626e5aa777791a
Jakub Trnčák <trncak@rtsoft.cz>
2023-02-21 09:55:49 +0100

    Complete Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navigation from './components/navigation/Navigation'
+import Homepage from './components/homepage/HomePage'
 import './index.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <section className='App'>
         <Navigation />
+        <Homepage />
     </section>
   )
 }
