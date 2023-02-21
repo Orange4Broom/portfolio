8cd052142f75a61a29d7d5b628efc378b7295b8c
Jakub Trnčák <trncak@rtsoft.cz>
2023-02-21 10:47:58 +0100

    Repair bugs and path for background image

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -1,4 +1,5 @@
 import AOS from 'aos';
+import LinkButton from '../linkbutton/LinkButton';
 import 'aos/dist/aos.css';
 import './homepage.css';
 
@@ -11,7 +12,7 @@ function Homepage() {
                 <h1 className='name-header'>Jakub <span className='padding-text'>Trnčák</span></h1>
                 <p className='info-paragraf'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer lacinia. Sed ac dolor sit amet purus. </p>
             </div>
-            <button data-aos="fade-up" className='next-button'>Find out more</button>
+                <LinkButton />
         </div>
     )
 }
