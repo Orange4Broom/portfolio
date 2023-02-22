612e07978dbc62413ea76d9c2b96d931864a838a
OrangeBroom <orangebroom@laptop-nkq6j1bk.home>
2023-02-22 01:26:47 +0100

    Comlpete sliding menu

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -12,9 +12,9 @@ function Contact() {
         <section className="Contact">
             <Navigation />
             <div className='links-wrapper'>
-                <div className='contact-links'>
-                    <a data-aos="fade-right" href="https://github.com/Orange4Broom" target="_blank" rel='noreferrer' className='github'><Icon name='github' type='fab' /><span className='icon-tag'>GitHub</span></a>
+                <div className='contact-links'>                  
                     <a data-aos="fade-up" href="https://discord.com/users/519971592076394531" target="_blank" rel='noreferrer' className='discord'><Icon name='discord' type='fab' /><span className='icon-tag'>Discord</span></a>
+                    <a data-aos="fade-right" href="https://github.com/Orange4Broom" target="_blank" rel='noreferrer' className='github'><Icon name='github' type='fab' /><span className='icon-tag'>GitHub</span></a>
                     <a data-aos="fade-left" href="https://twitter.com/Orange4Broom" target="_blank" rel='noreferrer' className='twitter'><Icon name='twitter' type='fab' /><span className='icon-tag'>Twitter</span></a>
                 </div>
             </div>
