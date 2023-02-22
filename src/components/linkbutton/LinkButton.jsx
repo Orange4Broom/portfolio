4e56993092d0d8814f3e7ff7bcfd3c55b657e018
Jakub Trnčák <trncak@rtsoft.cz>
2023-02-22 07:46:07 +0100

    Fix link-button bug

--- src/components/linkbutton/LinkButton.jsx
+++ src/components/linkbutton/LinkButton.jsx
@@ -4,7 +4,9 @@ import './linkbutton.css';
 function LinkButton() {
     return (
         <>
-            <Link to='About'><button data-aos="fade-up"  data-aos-offset="0" className='next-button'>Find out more</button></Link>
+            <Link className='link-button' to='About'>
+                <button data-aos="fade-up"  data-aos-offset="0" className='next-button'>Find out more</button>
+            </Link>
         </>
     )
 }
