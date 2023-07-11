5d8d3a3f90160f2d06b95517190cf14834653f23
Orange4Broom <orang4broom@gmail.com>
2023-07-11 08:41:14 +0200

    Change navigation styling

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -22,6 +22,8 @@ function Navigation() {
 
   const navigationStyle = {
     backgroundColor: scrolled ? 'rgba(0, 0, 0, 0.57)' : 'transparent',
+    borderBottom: scrolled ? '1px solid rgba(255, 255, 255, 0.2)' : 'none',
+    backdropFilter: scrolled ? 'blur(24px)' : 'none',
     transition: 'background-color 0.4s ease',
   };
 
